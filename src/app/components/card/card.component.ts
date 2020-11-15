4c0828a6d5734cd47731e1802bc5ff39a0f240c2
amir4rab <60965542+amir4rab@users.noreply.github.com>
2020-11-15 17:19:37 +0330

    slider-start

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Chart } from 'Chart.js';
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,7 +9,36 @@ import { environment } from '../../../environments/environment';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
+  @Input() inputData: object;
+  dataArr: number[] = [
+    15348.191231599,
+    15446.6300066755,
+    15567.0743339388,
+    15484.058229308,
+    15378.0037113131,
+    15354.603654194,
+    15322.8169880141,
+    15478.3381936886,
+    15472.7197781632,
+    15545.9708002348,
+    15511.7680989959,
+    15498.3894633605,
+    15437.5401567316,
+    15390.2600613415,
+    15439.7005471411,
+    15409.0349090621,
+    15384.8589856469,
+    15298.3689155139,
+    15423.1713527891,
+    15448.9392916731,
+    15505.8539901769,
+    15632.1108884373,
+    15404.7822698933,
+    15298.4873130554,
+    15140.775243554,
+    14974.2673279431,
+    15021.5036275302
+  ];
   constructor(  ) { }
 
   ngOnInit(): void { 
@@ -18,49 +48,20 @@ export class CardComponent implements OnInit {
   makeChart(): void{
     const canvas = (document.getElementById('btcCanvas') as HTMLCanvasElement).getContext('2d');
 
-    const rgbaBgColor = 'rgba(63, 63, 63, 0)';
-    const rgbaColor = 'rgba(47, 244, 102, 1)';
-    const dataArr: number[] = [
-      15348.191231599,
-      15446.6300066755,
-      15567.0743339388,
-      15484.058229308,
-      15378.0037113131,
-      15354.603654194,
-      15322.8169880141,
-      15478.3381936886,
-      15472.7197781632,
-      15545.9708002348,
-      15511.7680989959,
-      15498.3894633605,
-      15437.5401567316,
-      15390.2600613415,
-      15439.7005471411,
-      15409.0349090621,
-      15384.8589856469,
-      15298.3689155139,
-      15423.1713527891,
-      15448.9392916731,
-      15505.8539901769,
-      15632.1108884373,
-      15404.7822698933,
-      15298.4873130554,
-      15140.775243554,
-      14974.2673279431,
-      15021.5036275302
-    ];
+    const bgColor = 'rgba(63, 63, 63, 0)';
+    const color = 'rgba(47, 244, 102, 1)';
 
     const myChart = new Chart(canvas, {
       type: 'line',
       data: {
-        labels: dataArr,
+        labels: this.dataArr,
         datasets: [{
-          data: dataArr,
+          data: this.dataArr,
           borderColor: [
-            rgbaColor
+            color
           ],
           backgroundColor:[
-            rgbaBgColor
+            bgColor
           ],
           pointStyle: 'line',
           pointRadius: 0,
@@ -72,4 +73,11 @@ export class CardComponent implements OnInit {
     });
   }
 
+  strArrToNumArr( inputArr: string[] ): number[]{
+    const outputArr: number[] = [];
+
+    inputArr.forEach(str => outputArr.push(parseInt(str)));
+
+    return outputArr;
+  }
 }
