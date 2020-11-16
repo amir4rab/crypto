c608dd62d2861b56d21b314d973e5dab9da59fc2
amir4rab <60965542+amir4rab@users.noreply.github.com>
2020-11-16 12:57:25 +0330

    drag-slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent implements OnInit, AfterViewInit {
+
+  sliderId: string = "slider" + (Math.random() * 10000).toFixed(0);
+
+  el:HTMLElement;
+  keyDown: Boolean = false;
+  startX: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    // console.log(this.sliderId);
   }
 
+  ngAfterViewInit(): void{
+    this.el = document.getElementById(this.sliderId) as HTMLElement;
+  }
+
+  dragFn(event: MouseEvent, mState: boolean, setX: boolean = false): void {
+    this.keyDown = mState;
+    if ( setX ) this.startX = event.pageX + this.el.scrollLeft;
+  }
+
+  MoveFn(event: MouseEvent): void {
+    if( this.keyDown ) this.el.scrollLeft = -(event.pageX - this.startX);
+  }
 }
